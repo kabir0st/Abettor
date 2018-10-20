ddb4abc69d46601e363ebbdc44a8410f89aad42a
Kabir S. Tamari <rabbit.bonex@gmail.com>
2018-10-20 13:10:01 +0545

    reinit

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.user_login, name = 'login'),
-    path('new',views.student_signup, name = 'register new students'),
-    path('logout',views.user_logout, name = 'Logout'),
-]
