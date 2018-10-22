37ab796a4d9830aa703e8d28b3de9cb8601b6ffd
Kabir S. Tamari <rabbit.bonex@gmail.com>
2018-10-23 00:14:48 +0545

    still working on dashbaord

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def dashboard(request):
-    return HttpResponse('dashboard')
+    return render(request, 'dashboard/dashboard.html')
+
     
