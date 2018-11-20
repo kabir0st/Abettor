fe8ae06ae299983cb0d36c46f13e1ba445ab83ac
Kabir Tamari <rabbit.bonex@gmail.com>
2018-11-21 04:35:23 +0545

    extended fixtures

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Student,Semester,CustomUser, FeeTable
+from .models import Student,Semester,CustomUser, FeeTable,Subject
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Student)
 admin.site.register(Semester)
 admin.site.register(FeeTable)
+admin.site.register(Subject)
 
