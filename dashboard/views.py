5784cc8e7f0db381041f5d034f75664c1d500468
Kabir Tamari <rabbit.bonex@gmail.com>
2018-11-27 17:27:34 +0545

    added qr generator for testing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from user.models import Student
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import qrcode
+import json
+import os
+
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    if request.method == "POST":
+        json_str = request.body.decode(encoding='UTF-8')
+        json_obj = json.loads(json_str)
+        uuid = json_obj['uuid']
+        qr = qrcode.QRCode(
+            version = 1,
+            error_correction = qrcode.constants.ERROR_CORRECT_H,
+            box_size = 10,
+            border = 4,
+        )
+        data = uuid
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image()
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        x = (os.path.join(BASE_DIR, 'dashboard\static\qr.jpg'))
+        img.save(x)
+        response_json = {'good':True}
+        return HttpResponse(json.dumps(response_json),content_type = 'application/json')
 
+    else:
+        return render(request, 'dashboard/dashboard.html')
