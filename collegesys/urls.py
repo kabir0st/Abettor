2f017f729f46a1ba63faeeb7cec05a38b093fd1a
Kabir Tamari <rabbit.bonex@gmail.com>
2018-12-18 02:14:01 +0545

    added some urls

--- collegesys/urls.py
+++ collegesys/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('main.urls')),
-    path(' ',include('main.urls')),
+    path('',include('main.urls')),
     path('user/',include('user.urls')),
     path('dashboard/',include('dashboard.urls')),
     path('payment/',include('payment.urls')),
