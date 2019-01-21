330ba781bb5fb71c3203958c0c6d59a431bac4c0
Kabir Tamari <rabbit.bonex@gmail.com>
2019-01-21 21:06:10 +0545

    few changes

--- de.py
+++ de.py
@@ -1,26 +1,17 @@
 import qrcode
 
-# Create qr code instance
-qr = qrcode.QRCode(
+def run():
+    qr = qrcode.QRCode(
     version = 1,
     error_correction = qrcode.constants.ERROR_CORRECT_H,
     box_size = 10,
     border = 4,
-)
+    )
+    data = "9559ebf7fa3f4e6d83ea504242bd8415"
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save("image.jpg")
 
-# The data that you want to store
-# data = input('Enter UUID :')
-data = "52dfdae6bf78471d84314c3eee8a4a84"
-
-# Add data
-qr.add_data(data)
-qr.make(fit=True)
-
-# Create an image from the QR Code instance
-img = qr.make_image()
-
-# Save it somewhere, change the extension as needed:
-# img.save("image.png")
-# img.save("image.bmp")
-# img.save("image.jpeg")
-img.save("image.jpg")
+if __name__ == "__main__":
+    run()
